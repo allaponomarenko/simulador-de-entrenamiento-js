2d8473c9530079d87391e27c40a4dc62b28d68fd
allaponomarenko <alla.deyneka03@gmail.com>
2024-03-13 10:10:25 +0100

    куку

---
+++ js/mitask-1.js
@@ -0,0 +1,10 @@
+//оголошення операторів
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+//отримати доступ до CSS за допомогою об’єкта документа, який представляє весь ваш документ CSS
+const button1 = document.querySelector("#button1");
