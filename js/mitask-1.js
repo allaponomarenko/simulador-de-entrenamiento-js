cc2a3d18ff827c2e521539e4231e4cbd18743e8d
allaponomarenko <alla.deyneka03@gmail.com>
2024-03-14 16:12:11 +0100

    ccc

--- js/mitask-1.js
+++ js/mitask-1.js
@@ -1,11 +1,11 @@
-//оголошення операторів
+//оголошення операторів. Оголошення змінних:
 let xp = 0;
 let health = 100;
 let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
-let inventory = ["stick"];
+
 //отримати доступ до CSS за допомогою об’єкта документа, який представляє весь ваш документ CSS
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
@@ -36,14 +36,8 @@ function fightDragon() {
   console.log("Fighting dragon.");
 }
 
-function buyHealth() {
+function buyHealth() {}
 
-}
+function buyWeapon() {}
 
-function buyWeapon() {
-
-}
-
-function goTown() {
-
-}
+function goTown() {}
